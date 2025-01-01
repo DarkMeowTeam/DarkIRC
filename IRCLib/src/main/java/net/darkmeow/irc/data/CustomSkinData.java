4541099253ae7e2231bad52d84b1763b610e3f52
NekoCurit <108512490+NekoCurit@users.noreply.github.com>
2025-01-01 19:21:14 +0800

    refactor(Protocol): CustomSkinData spell

--- IRCLib/src/main/java/net/darkmeow/irc/data/CustomSkinData.java
+++ IRCLib/src/main/java/net/darkmeow/irc/data/CustomSkinData.java
@@ -8,25 +8,28 @@ public class CustomSkinData {
      */
     @Nullable
     public final String skin;
+
     /**
      * 披风
      */
     @Nullable
     public final String cape;
+
     /**
      * 皮肤是否为纤细模式 (仅1.8+ minecraft 客户端)
      */
-    public final boolean isSlime;
+    public final boolean isSlim;
+
     /**
      * 自定义皮肤数据
      *
      * @param skin 皮肤
      * @param cape 披风
-     * @param isSlime 皮肤是否为纤细模式 (仅1.8+ minecraft 客户端)
+     * @param isSlim 皮肤是否为纤细模式 (仅1.8+ minecraft 客户端)
      */
-    public CustomSkinData(@Nullable String skin, @Nullable String cape, boolean isSlime) {
+    public CustomSkinData(@Nullable String skin, @Nullable String cape, boolean isSlim) {
         this.skin = skin;
         this.cape = cape;
-        this.isSlime = isSlime;
+        this.isSlim = isSlim;
     }
 }
