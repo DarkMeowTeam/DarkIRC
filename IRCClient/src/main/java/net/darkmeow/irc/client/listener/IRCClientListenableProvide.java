db36686dabe7c90788a26d424658ffac4132bca9
NekoCurit <108512490+NekoCurit@users.noreply.github.com>
2025-01-13 01:30:58 +0800

    doc(Client): listenable provide

--- IRCClient/src/main/java/net/darkmeow/irc/client/listener/IRCClientListenableProvide.java
+++ IRCClient/src/main/java/net/darkmeow/irc/client/listener/IRCClientListenableProvide.java
@@ -3,19 +3,58 @@ package net.darkmeow.irc.client.listener;
 import net.darkmeow.irc.client.enums.EnumDisconnectType;
 import net.darkmeow.irc.client.interfaces.data.IRCDataOtherSessionInfo;
 import net.darkmeow.irc.client.interfaces.data.IRCDataSelfSessionInfo;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface IRCClientListenableProvide {
 
-    void onUpdateSession(String token);
+    /**
+     * 接收到新的登录凭据(token)时调用
+     * 在这里编写保存凭据部分代码并下次登录使用用户名+凭据登录而不是用户名+密码
+     *
+     * @param token 凭据
+     */
+    void onUpdateSession(@NotNull String token);
 
-    void onUpdateUserInfo(IRCDataSelfSessionInfo info, boolean isFirstLogin);
+    /**
+     * 当从服务端接收到自身信息时调用
+     * 包括 首次登录成功, 收到服务端更新头衔, 权限等级变更..
+     *
+     * @param info 信息
+     * @param isFirstLogin 是否为首次登录成功
+     */
+    void onUpdateUserInfo(@NotNull IRCDataSelfSessionInfo info, boolean isFirstLogin);
 
-    void onMessagePublic(IRCDataOtherSessionInfo sender, String message);
+    /**
+     * 收到公共聊天时调用
+     *
+     * @param sender 发送者数据
+     * @param message 消息内容
+     */
+    void onMessagePublic(@NotNull IRCDataOtherSessionInfo sender, @NotNull String message);
 
-    void onMessagePrivate(IRCDataOtherSessionInfo sender, String message);
+    /**
+     * 收到私有聊天时调用
+     *
+     * @param sender 发送者数据
+     * @param message 消息内容
+     */
+    void onMessagePrivate(@NotNull IRCDataOtherSessionInfo sender, @NotNull String message);
 
-    void onMessageSystem(String message);
+    /**
+     * 收到系统消息调用
+     *
+     * @param message 消息内容
+     */
+    void onMessageSystem(@NotNull String message);
 
-    void onDisconnect(EnumDisconnectType type, String reason, boolean logout);
+    /**
+     * 与 IRC 服务器连接中断时调用
+     *
+     * @param type 类型  意外断开/被服务端踢出/用户主动断开
+     * @param reason 原因  只有在被服务端踢出时才有
+     * @param logout 退出登录  凭据失效
+     */
+    void onDisconnect(@NotNull EnumDisconnectType type, @Nullable String reason, boolean logout);
 
 }
