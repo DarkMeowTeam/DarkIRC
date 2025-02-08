79ab6c975357d1cd041d8b8e11c71359e71c7e7f
NekoCurit <108512490+NekoCurit@users.noreply.github.com>
2025-02-09 03:26:00 +0800

    perf: less thread, bytebuf allocator, packet size limit

--- IRCLib/src/main/java/net/darkmeow/irc/network/IRCNetworkBaseConfig.java
+++ IRCLib/src/main/java/net/darkmeow/irc/network/IRCNetworkBaseConfig.java
@@ -1,5 +1,5 @@
 package net.darkmeow.irc.network;
 
 public class IRCNetworkBaseConfig {
-    public static final int MAX_FRAME_LENGTH = 1048576; // 1MB
+    public static final int MAX_FRAME_LENGTH = 524288; // 0.5MB
 }
