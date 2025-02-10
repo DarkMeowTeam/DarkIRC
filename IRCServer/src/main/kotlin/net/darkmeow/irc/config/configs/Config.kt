bcb299ea0c16855cd96bf0e4f5ba55afcc976f5a
NekoCurit <108512490+NekoCurit@users.noreply.github.com>
2025-02-10 21:33:57 +0800

    refactor(Server): bind host, update config

--- IRCServer/src/main/kotlin/net/darkmeow/irc/config/configs/Config.kt
+++ IRCServer/src/main/kotlin/net/darkmeow/irc/config/configs/Config.kt
@@ -2,24 +2,34 @@ package net.darkmeow.irc.config.configs
 
 @Suppress("Unused")
 class Config {
-    /**
-     * 服务器监听端口
-     */
-    var port: Int = 8080
-    /**
-     * 是否启用 Proxy Protocol
-     */
-    var proxyProtocol: Boolean = false
-    /**
-     * 服务器连接密钥
-     */
-    var key: String = "publicIRCTest123"
+
     /**
      * 存储用户数据的数据库
      */
     var database: String = "jdbc:sqlite:data.db"
 
     var userLimit: UserLimit = UserLimit()
+
+    var ircServer = IRCServer()
+
+    class IRCServer {
+        /**
+         * 服务器监听 IP
+         */
+        var host = "0.0.0.0"
+        /**
+         * 服务器监听端口
+         */
+        var port = 8080
+        /**
+         * 是否启用 Proxy Protocol
+         */
+        var proxyProtocol: Boolean = false
+        /**
+         * 服务器连接密钥
+         */
+        var key: String = "publicIRCTest123"
+    }
 
     class UserLimit {
         /**
@@ -39,7 +49,6 @@ class Config {
          * Web API 密钥
          */
         var key = ""
-
         /**
          * Web API IP 白名单
          * 留空关闭
