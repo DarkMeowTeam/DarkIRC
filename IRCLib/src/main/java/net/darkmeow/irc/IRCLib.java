ea72c228e7e04326dc1ec9ee924412ce29567d2a
NekoCurit <108512490+NekoCurit@users.noreply.github.com>
2025-05-05 10:27:39 +0800

    refactor(lib): reset protocol_version to 0

--- IRCLib/src/main/java/net/darkmeow/irc/IRCLib.java
+++ IRCLib/src/main/java/net/darkmeow/irc/IRCLib.java
@@ -4,9 +4,6 @@ public class IRCLib {
     /**
      * IRC协议版本号
      * 只有在更新版本无法向下兼容时才会递增
-     * 更新历史:
-     * 1 -> 单用户多会话兼容
-     * 2 -> 客户端信息与客户端标识数据分开传输
      */
-    public static final int PROTOCOL_VERSION = 2;
+    public static final int PROTOCOL_VERSION = 0;
 }
