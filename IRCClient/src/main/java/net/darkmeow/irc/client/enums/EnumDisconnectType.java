6f53a9559654812103dfc22d5e00e8343ae0a075
NekoCurit <108512490+NekoCurit@users.noreply.github.com>
2025-05-05 13:08:16 +0800

    refactor(client\base): add disconnect type

--- IRCClient/src/main/java/net/darkmeow/irc/client/enums/EnumDisconnectType.java
+++ IRCClient/src/main/java/net/darkmeow/irc/client/enums/EnumDisconnectType.java
@@ -6,11 +6,16 @@ public enum EnumDisconnectType {
      */
     DISCONNECT_BY_USER,
     /**
+     * 登录失败
+     */
+    FAILED_TO_LOGIN,
+    /**
      * 被服务器踢出
      */
     KICK_BY_SERVER,
     /**
      * 其它
+     * 主要为意外断开连接
      */
     OTHER
 }
