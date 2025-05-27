4a29a398fece5dcc5cf1c17710d704d3a00278a2
NekoCurit <108512490+NekoCurit@users.noreply.github.com>
2025-05-27 18:15:41 +0800

    feat(server): auto create default client and admin account

--- IRCServer/src/main/kotlin/net/darkmeow/irc/database/DataBaseManager.kt
+++ IRCServer/src/main/kotlin/net/darkmeow/irc/database/DataBaseManager.kt
@@ -1,10 +1,16 @@
 package net.darkmeow.irc.database
 
 import net.darkmeow.irc.IRCServer
+import net.darkmeow.irc.data.base.DataClient
+import net.darkmeow.irc.data.enmus.EnumUserPremium
 import net.darkmeow.irc.database.data.DataBaseClient
 import net.darkmeow.irc.database.data.DataBaseSession
 import net.darkmeow.irc.database.data.DataBaseUser
 import net.darkmeow.irc.database.data.DataBaseUserdata
+import net.darkmeow.irc.database.extensions.DataManagerClientExtensions.createClient
+import net.darkmeow.irc.database.extensions.DataManagerClientExtensions.getClients
+import net.darkmeow.irc.database.extensions.DataManagerUserExtensions.createUser
+import net.darkmeow.irc.database.extensions.DataManagerUserExtensions.getUsers
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -28,7 +34,20 @@ class DataBaseManager(
             SchemaUtils.create(DataBaseUser, DataBaseUserdata, DataBaseSession, DataBaseClient)
         }
 
+        createDefault()
         base.logger.info("[数据库管理] 初始化成功.")
+    }
+
+    fun createDefault() {
+        println(getClients().size)
+        if (this.getClients().isEmpty() && this.getUsers().isEmpty()) {
+            this.createClient(name = "default", metadata = DataClient.ClientMetadata(allowLoginMinVersion = 0)).also {
+                base.logger.info("[数据库管理] 创建默认登录客户端信息(name: default, key: ${it.key})")
+            }
+            this.createUser(name = "Administrator", password = "123456", premium = EnumUserPremium.OWNER).also {
+                base.logger.info("[数据库管理] 创建默认用户(name: Administrator, password: 123456)")
+            }
+        }
     }
 
 }
